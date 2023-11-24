940297a3586dabffe6f97e51f700a471fe017e91
bruk-lemma <bruklemma2017@gmail.com>
2023-11-24 16:13:35 +0300

    retrive item by id endpoint

--- main.go
+++ main.go
@@ -27,6 +27,19 @@ func postAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+	// loop over the list albums and return the album if found
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	}
+
+}
+
 // albums slice to seed record album data.
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
@@ -37,6 +50,7 @@ var albums = []album{
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8000")
